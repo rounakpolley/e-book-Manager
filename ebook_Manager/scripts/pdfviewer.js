7737b7ff83d570442f602f04963e48df669efd70
Rounak Polley <rounakpolley19972014@gmail.com>
2018-04-25 01:40:19 +0530

    added download button and hidden input fields in make-new-note form

--- ebook_Manager/scripts/pdfviewer.js
+++ ebook_Manager/scripts/pdfviewer.js
@@ -227,12 +227,15 @@ $(document).ready(function(){
 	document.getElementById("make-note").onclick = function(){
 		//log % scroll
 		if(file_open){
+			scroll_percentage = get_scroll_percentage();
+			$('#scroll-percentage').val(scroll_percentage);
+			$('#canvas-orientation').val(rotation);
+			//---- next line is only for testing purposes
+			console.log($('#scroll-percentage').val(),"|",$('#canvas-orientation').val());
+
 			$('#show-catalogue-note').addClass('hidden');
 			$('#new-note-form').removeClass('hidden');
-			//-------------------------------- comment next line later function meant to be called from php
-			scroll_percentage = get_scroll_percentage();
-			//---- next line is only for testing purposes
-			console.log(scroll_percentage);
+			
 			document.getElementById("notes-drawer").style.width = "24.5vw";
 		}
 		else{
